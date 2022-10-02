4a2f823872fdc9bb093e0e0588a0a2532c2b94e0
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-02 17:25:17 -0400

    script directory update

--- scripts/docker_gkserver_backup.sh
+++ scripts/docker_gkserver_backup.sh
@@ -9,12 +9,12 @@ echo $(date) >> $logdest
 echo "" >> $logdest
 
 #stop docker
-/usr/local/bin/docker compose -f /home/kepler/gkserver/docker-compose/docker-compose.yml stop
+/usr/bin/docker compose -f /home/kepler/gkserver/docker-compose/docker-compose.yml stop
 
 rsync -avhi --delete --backup-dir=$trashdir $src1 $dest1 2>&1| tee -a $logdest
 
 #start docker
-/usr/local/bin/docker compose -f /home/kepler/gkserver/docker-compose/docker-compose.yml start
+/usr/bin/docker compose -f /home/kepler/gkserver/docker-compose/docker-compose.yml start
 
 echo "" >> $logdest
 echo $(date) >> $logdest
