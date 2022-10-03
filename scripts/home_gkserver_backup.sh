26c8a37b61a1e3bceec11bd16405c932fcec6808
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-03 06:12:05 -0400

    script directory update

--- scripts/home_gkserver_backup.sh
+++ scripts/home_gkserver_backup.sh
@@ -3,12 +3,13 @@
 logdest="/home/kepler/logs/home_gkserver_backup.log"
 src1="/home/kepler"
 dest1="/media/backup_main/gkserver_home"
+exclude1="/home/kepler/gkserver"
 trashdir="/media/backup_main/gkserver_home/gkserver_home_trash/$(date +%m-%d-%Y)"
 
 echo $(date) >> $logdest
 echo "" >> $logdest
 
-rsync -avhi --delete --exclude 'docker' --backup-dir=$trashdir $src1 $dest1 2>&1 | tee -a $logdest
+rsync -avhi --delete --exclude $exclude1 --backup-dir=$trashdir $src1 $dest1 2>&1 | tee -a $logdest
 
 echo "" >> $logdest
 echo $(date) >> $logdest
