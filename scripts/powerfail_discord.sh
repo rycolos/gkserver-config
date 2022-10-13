2e2436905a7f68de7378e74d605730aaae8cd895
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-13 07:50:22 -0400

    powerfail script update

--- scripts/powerfail_discord.sh
+++ scripts/powerfail_discord.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#configure pwrtstat to use this command with sudo pwrstat -pwrfail -cmd /home/gkserver/scripts/powerfail_discord.sh
+#configure pwrtstat to use this command with sudo pwrstat -pwrfail -cmd /home/kepler/gkserver/scripts/powerfail_discord.sh
 
 echo "Warning: Utility power failure has occurred for a while, system will be shutdown soon!" | wall
 echo "Warning: Utility power failure has occurred for a while, system will be shutdown soon!" | python3 /home/kepler/gkserver/scripts/discord-pipe.py
